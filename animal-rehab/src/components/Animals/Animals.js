a00de7ababc80389620f82caf67a335802639a54
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-02-10 17:55:26 -0800

    made some dummy data for medicine and logs, create animal, and delete functionality working

--- animal-rehab/src/components/Animals/Animals.js
+++ animal-rehab/src/components/Animals/Animals.js
@@ -1,33 +1,59 @@
 import React from "react";
 import axios from 'axios';
+import uuid from "uuid";
 
-function AnimalInCare(animals) {
+function AnimalInCare(props) {
 
   return (
     <>
       <li className="animals">
-        <p>{animals.animal.name}</p>
+        <p>{props.animal.name}: {props.animal.date}</p>
+      {<button id="delete"onClick={() => props.removeAnimal(props.index)}>Delete</button>}
+
       </li>
     </>
   )
 }
 
+
 class AnimalIntakeForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { animals: [] };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { 
+      animals: [],
+      date: new Date(),
+      name: ""
+    };
   }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  
+  addAnimal = animalName => {
+    const animal = {id: uuid.v4(), name: animalName, date: this.state.date.toDateString()}
+    this.setState(prevState => ({
+      animals: prevState.animals.concat(animal)
+    }));
+    this.setState({date: new Date()})
   }
+  
+  removeAnimal = index => {
+    const animals = this.state.animals;
+    animals.splice(index, 1);
+    this.setState({ animals });
+  };
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+  handleChange = (e) => {
+    this.setState({
+        name: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    if (this.state.name) {
+      this.addAnimal(this.state.name)
+      this.setState({
+          name: ""
+      });
+    }
+    e.preventDefault();
   }
 
   async componentDidMount() {
@@ -37,22 +63,28 @@ class AnimalIntakeForm extends React.Component {
     this.setState({
       animals: response.data
     })
-
   }
 
   render() {
     return (
+      
       <>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Animal:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
+        <h1>Animals currently in my care: {this.state.animals.length}</h1>
+
+        <form className="animalForm" onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.value} onChange={e => this.handleChange(e)} />
+            <input type="submit" value="Add Animal" />
         </form>
-        <h1>Animals currently in my care:</h1>
+
         <ul>
-          {this.state.animals.map(animals => <AnimalInCare key={animals.id} animal={animals} />)}
+          {this.state.animals.map((animals, index) => 
+            <AnimalInCare 
+                removeAnimal={this.removeAnimal} 
+                key={animals.id} 
+                animal={animals} 
+                index={index}
+              />
+            )}
         </ul>
       </>
     );
