c8706031012f1877c2bb30450a8bc6b285ca2e9d
Terrell Douglas <terrelloydouglas@yahoo.com>
2020-02-11 08:34:14 -0800

    footer done

--- animal-rehab/src/components/Footer/Footer.js
+++ animal-rehab/src/components/Footer/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 class Footer extends React.Component {
     render() {
         return <footer>
-            <p>Medical Disclaimer</p>
-            <p>All content found on this website, including: text, images, or other formats were created for educational purposes only. If you or someone else has found an animal in need or are looking for medical advice, please contact your nearest wildlife vet. We do not recommend you handle or care for wild animals without the proper licence and skills.</p>
+            <div className='footer'>
+                <p>Medical Disclaimer</p>
+                <p>All content found on this website, including: text, images, or other formats were created for educational purposes only. If you or someone else has found an animal in need or are looking for medical advice, please contact your nearest wildlife vet. We do not recommend you handle or care for wild animals without the proper licence and skills.</p>
+            </div>
         </footer>
     }
 }
