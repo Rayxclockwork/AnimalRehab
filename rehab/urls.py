c485fa6ca3fc343a33440aa1806e5d6b5c86c820
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-02-10 16:13:17 -0800

    DRF route serving over localhost

--- rehab/urls.py
+++ rehab/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import MedicineList, AnimalList, LogList
+from .views import MedicineList, AnimalList, LogList, UserAnimalsList
 
 urlpatterns = [
     path('medicine/', MedicineList.as_view(), name = 'medicine_list'),
-    path('animals/', AnimalList.as_view(), name = 'animal_list')
-
+    path('animals/', AnimalList.as_view(), name = 'animal_list'),
+    path('animals/<int:uid>/', UserAnimalsList.as_view(), name = 'user_animals_list'),
 ]
--- rehab/views.py
+++ rehab/views.py
@@ -1,18 +1,38 @@
+from django.contrib.auth.models import User
 from .models import Medicine, Animal, Log
 from .serializers import MedicineSerializer, AnimalSerializer, LogSerializer
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
+
+# Medicine views
 
 class MedicineList(generics.ListCreateAPIView):
     """Retrieving all medicines."""
     queryset = Medicine.objects.all()
     serializer_class = MedicineSerializer
 
+
+# Animal views
+
    
 class AnimalList(generics.ListCreateAPIView):
     """Retrieving all animals."""
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+
+
+class UserAnimalsList(generics.ListAPIView):
+    """List all animals of user."""
+    serializer_class = AnimalSerializer
+    
+    def get_queryset(self):
+        """Set of all animals of user."""
+        self.vet = get_object_or_404(User, id=self.kwargs['uid'])
+        return Animal.objects.filter(vet=self.vet)
+
+
+# Log views
 
 
 class LogList(generics.ListCreateAPIView):
