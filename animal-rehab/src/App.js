8d489cf154ebc060c137a564b69e4c91c6bedb56
RAVEN DELANEY <ravend17@gmail.com>
2020-02-12 10:12:03 -0800

    drop downs added to dosage form

--- animal-rehab/src/App.js
+++ animal-rehab/src/App.js
@@ -15,23 +15,14 @@ import AnimalProfile from './components/AnimalProfile/AnimalProfile';
 import Animals from './components/Animals/Animals';
 import LogIn from './components/LogIn/LogIn';
 import Footer from './components/Footer/Footer';
-<<<<<<< HEAD
 import Header from './components/Header/Header';
-
-=======
-
-import AnimalDetails from './components/AnimalDetails/AnimalDetails'
->>>>>>> dd0979f82dc289e3d10cf0aa1b1fcc03ffb9a0ce
 import './App.scss';
 import './components/Home/Home.scss';
 import './components/Dose/Dose.scss';
 import './components/Medicine/Medicine.scss';
 import './components/Animals/Animals.scss';
 import './components/Footer/Footer.scss';
-<<<<<<< HEAD
 import './components/Header/Header.scss';
-=======
->>>>>>> dd0979f82dc289e3d10cf0aa1b1fcc03ffb9a0ce
 import './components/LogIn/LogIn.scss';
 
 
@@ -94,14 +85,9 @@ class App extends React.Component {
                 <Route path="/animals/:aid">
                   <AnimalProfile animals={this.state.animals} />
                 </Route>
-<<<<<<< HEAD
-                <Route path="/animals/:aid" render={this.animalProfile} />
-=======
 
 
                 <Route path="/animals/:aid" render={this.animalProfile} />
-
->>>>>>> dd0979f82dc289e3d10cf0aa1b1fcc03ffb9a0ce
                 <Route path="/log">
                   <LogIn />
                 </Route>
