3d7d9092dd51b349b878dc601f4ad7b9d9e525ee
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-02-11 16:06:14 -0800

    Animal profile component created

---
+++ animal-rehab/src/components/AnimalProfile/AnimalProfile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+
+
+export default props => {
+  const { aid } = useParams();
+  const animal = props.animals.find(animal => animal.id === parseInt(aid));
+
+  if (animal) {
+    return (
+      <>
+      <h2>{animal.name}</h2>
+      <p>{`${animal.entryAt} - ${animal.exitAt}`}</p>
+      <h3>Medication Details</h3>
+      <p>{animal.details}</p>
+      </>
+    );
+  } else {
+    return <Redirect to="/animals" />;
+  }
+};
