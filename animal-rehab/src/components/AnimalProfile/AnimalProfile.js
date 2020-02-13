2de48a3db5ef3e1689839118adbaa993de45b2f3
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-02-13 14:01:44 -0800

    Logs can be created for animal profile

--- animal-rehab/src/components/AnimalProfile/AnimalProfile.js
+++ animal-rehab/src/components/AnimalProfile/AnimalProfile.js
@@ -2,8 +2,8 @@ import React from 'react';
 import AnimalProfileChild from './AnimalProfileChild'
 
 
-class AnimalProfile extends React.Component{
-  constructor(props){
+class AnimalProfile extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       logDetails: [],
@@ -14,12 +14,12 @@ class AnimalProfile extends React.Component{
 
   logCreateHandler(event, logDetail) {
     event.preventDefault();
-    const sortedLogs = this.state.logDetails.sort((a,b) => a.id < b.id)
+    const sortedLogs = this.state.logDetails.sort((a, b) => a.id < b.id)
     let newId
-    if (sortedLogs[0]){
-      newId = sortedLogs[sortedLogs.length-1].id
-    } 
-    else{
+    if (sortedLogs[0]) {
+      newId = sortedLogs[sortedLogs.length - 1].id
+    }
+    else {
       newId = 1
     }
     const newLog = {
@@ -28,19 +28,19 @@ class AnimalProfile extends React.Component{
       id: newId
     }
     this.setState({
-        logDetails: this.state.logDetails.concat([newLog])
-    }, ()=> console.log(this.state.logDetails))
+      logDetails: this.state.logDetails.concat([newLog])
+    }, () => console.log(this.state.logDetails))
 
   }
-  // submitLog(event){
-  //   event.preventDefault();
-    
-  //   this.state.onSubmit(this.state.logDetail);
-  // }
-  
-  render(){
-    return<AnimalProfileChild logDetails = {this.state.logDetails} animals={this.props.animals} logCreateHandler ={this.logCreateHandler}/>
-}
+
+  render() {
+    return (
+      <AnimalProfileChild
+        logDetails={this.state.logDetails}
+        animals={this.props.animals}
+        logCreateHandler={this.logCreateHandler} />
+    );
+  }
 
 }
 export default AnimalProfile
--- animal-rehab/src/components/AnimalProfile/AnimalProfileChild.js
+++ animal-rehab/src/components/AnimalProfile/AnimalProfileChild.js
@@ -4,22 +4,30 @@ import AnimalProfileForm from './AnimalProfileForm';
 
 export default props => {
 
-    const { aid } = useParams();
-    const animal = props.animals.find(animal => animal.id === parseInt(aid));
+  const { aid } = useParams();
+  const animal = props.animals.find(animal => animal.id === parseInt(aid));
 
-    if (animal) {
-        return (
-          <>
-          <h2>{animal.name}</h2>
-          <p>{`${animal.entry_at} - ${animal.exit_at}`}</p>   
-          <h3>Data Log Details</h3>
-          <p>{animal.logDetails}</p>
-          <AnimalProfileForm logCreateHandler = {props.logCreateHandler}/>
-    
-          </>
-        );
-      } else {
-        return <Redirect to="/animals" />;
-      }
+  if (animal) {
+    return (
+      <>
+        <h2>{animal.name}</h2>
+        <p>{`${animal.entry_at} - ${animal.exit_at}`}</p>
+        <h3>Data Log Details</h3>
+        <AnimalProfileForm logCreateHandler={props.logCreateHandler} />
+        <section>
+          {props.logDetails.map(logDetail => {
+            return (
+              <section>
+                <p>{logDetail.date}</p>
+                <p>{logDetail.description}</p>
+              </section>
+            );
+          })}
+        </section>
+      </>
+    );
+  } else {
+    return <Redirect to="/animals" />;
+  }
 }
 
