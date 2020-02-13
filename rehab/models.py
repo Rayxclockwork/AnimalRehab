85cac4bbe00e5f9c0914f6e68b57f2cdb321057c
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-02-13 12:05:32 -0800

    fixed exit_at in models

--- rehab/models.py
+++ rehab/models.py
@@ -19,7 +19,7 @@ class Animal(models.Model):
     name = models.CharField(max_length=31)
     weight = models.FloatField()
     entry_at = models.DateTimeField(auto_now_add=True)
-    exit_at = models.DateTimeField()
+    exit_at = models.DateTimeField(auto_now=True)
     details = models.TextField()
     is_archived = models.BooleanField()
 
